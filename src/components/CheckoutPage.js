41d6f3cbc4aca7816e0cdac428a0deb9fc3e0340
Brayarn <brayarn.oduor@student.moringaschool.com>
2023-01-18 18:29:56 +0300

    modify form

--- src/components/CheckoutPage.js
+++ src/components/CheckoutPage.js
@@ -2,34 +2,36 @@ import "./CheckoutPage.css";
 
 const CheckoutPage = ({ handleClose }) => {
   return (
-    <div className="popup-box">
-      <div className="box">
-        <span className="close-icon" onClick={handleClose}>
-          x
-        </span>
-        <div className="payment-box">
-          <h3>You are one step to attending this event</h3>
-          <div className="payment-details">
-            <div className="ticket-details">
-              <label className="form-label">Ticket Amount</label>
-              <p>Price: Sh.10</p>
+    <form>
+      <div className="popup-box">
+        <div className="box">
+          <span className="close-icon" onClick={handleClose}>
+            x
+          </span>
+          <div className="payment-box">
+            <h3>You are one step to attending this event</h3>
+            <div className="payment-details">
+              <div className="ticket-details">
+                <label className="form-label">Ticket Amount</label>
+                <p>Price: Sh.10</p>
+              </div>
+              <label className="form-label">Phone Number</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your phone number"
+              />
+              <div className="form-text">
+                We'll never share your phone number with anyone else.
+              </div>
             </div>
-            <label className="form-label">Phone Number</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter your phone number"
-            />
-            <div className="form-text">
-              We'll never share your phone number with anyone else.
-            </div>
+            <button type="submit" className="btn btn-primary">
+              Make Payment
+            </button>
           </div>
-          <button type="submit" className="btn btn-primary">
-            Make Payment
-          </button>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 export default CheckoutPage;
